9a830448516b9ddde5ceac0ab4f3c4a15d203814
Ian <ian.lc0018@outlook.com.br>
2021-03-10 21:34:31 -0300

    Add files via upload

---
+++ aula_19.py
@@ -0,0 +1,93 @@
+# HOJE FALAREMOS SOBRE DICIONÁRIOS!
+#
+# RECAPITULANDO:
+#
+# dados = list()
+# dados.append('Pedro')
+# dados.append(25)
+# print(dados[0])   Pedro
+# print(dados[1])   25
+# ---------------------------------
+# - - - - -- DICIONÁRIOS -- - - - -
+#
+# Os Dicionários são identificados
+# pelas chaves "{" e "}".
+# Exemplos:
+#
+# dados = dict()
+# dados = {}
+# --------------------------
+# dados = {'nome':'Pedro', 'idade':25}
+# print(dados['nome'])  Pedro
+# print(dados['idade']) 25
+# ---------------------------------------------------
+# Quando se quer adicionar um novo elemento
+# a um dicionário, basta fazer assim:
+#
+# dados['novo_elemento'] = 'valor_do_novo_elemento'
+# ---------------------------------------------------
+# dados['sexo'] = 'M'
+# ---------------------------------------------------
+# Para deletar um dicionário ou
+# um elemento de um dicionário, basta
+# usar o comando del que foi visto nas
+# aulas anteriores.
+# Exemplo:
+#
+# del dados['nome_do_elemento']
+# ---------------------------------------------------
+# del dados['idade']
+# ---------------------------------------------------
+# filme = {'titulo':'Star Wars',
+#          'ano':1977,
+#          'diretor':'George Lucas'
+#         }
+# print(filme.values()) Mostra os Valores do dicionário "filme"
+# print(filme.keys()) Mostra as Chaves do dicionário "filme"
+# print(filme.items()) Mostra os Itens do dicionário "filme"
+# for k, v in filme.items():
+#     print(f'O {k} é {v}')      O titulo é Star Wars
+#                                O ano é 1977
+#                                O diretor é George Lucas
+
+pessoas = {'nome': 'Gustavo', 'sexo': 'M', 'idade': 22}
+print(f'O {pessoas["nome"]}, do sexo {pessoas["sexo"]} tem {pessoas["idade"]} anos.')
+print('----------------------------------------------------------------')
+print(pessoas.keys())
+print(pessoas.values())
+print(pessoas.items())
+print('----------------------------------------------------------------')
+# del pessoas['sexo']
+# pessoas['nome'] = 'Leandro'
+pessoas['peso'] = 98.5
+for k, v in pessoas.items():
+    print(f'{k} = {v}')
+
+print('----------------------------------------------------------------')
+brasil = list()
+estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
+estado2 = {'uf': 'São Paulo', 'sigla': 'SP'}
+brasil.append(estado1)
+brasil.append(estado2)
+
+print(brasil)
+print(brasil[0])
+print(brasil[1])
+print(brasil[0]['uf'])
+print(brasil[1]['sigla'])
+del brasil
+print('----------------------------------------------------------------')
+estado = dict()
+brasil = list()
+for c in range(0, 3):
+    estado['uf'] = str(input('Unidade Federativa: ')).strip()
+    estado['sigla'] = str(input('Sigla do Estado: ')).strip()
+    brasil.append(estado.copy())  # Copia todos os elementos do
+for e in brasil:                  # dicionário estado para a lista
+    for k, v in e.items():        # brasil
+        print(f'O campo {k} tem valor {v}')
+print()
+for e in brasil:
+    for v in e.values():
+        print(v, end=' ')
+    print()
