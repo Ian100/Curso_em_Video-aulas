0e6d7e380b2767579f4fc238535145630d5531ca
Ian <ian.lc0018@outlook.com.br>
2021-03-25 12:17:44 -0300

    Add files via upload

--- aula_20.py
+++ aula_20.py
@@ -20,6 +20,31 @@ def título(txt):
     lin()
 
 
+# EMPACOTAMENTO E DESEMPACOTAMENTO DE PARÂMETROS:
+#
+# A maioria das linguagens de programação
+# não permitem a contagem de parâmetros dentro de uma mesma função!
+# O Python permite! Isso é chamado de EMPACOTAMENTO!
+# Ex:
+#    def contador(* núm):
+#        _ Aqui vai o seu código! _
+#
+#    Obs.: O '*' é o símbolo para desempacotar,
+#          ou seja, ele está dizendo ao python
+#          para jogar todos os parâmetros do usuário
+#          na variável núm, sejam eles quais forem.
+def contador(* núm):
+    tam = len(núm)
+    print(f'Para os números {núm} existem {tam} valores.')
+
+
+def dobra(lst):
+    pos = 0
+    while pos < len(lst):
+        lst[pos] *= 2
+        pos += 1
+
+
 # Programa Principal 1
 lin()
 print(f'{"CURSO EM VÍDEO":^30}')
@@ -33,3 +58,14 @@ lin()
 título('CURSO EM VÍDEO')
 título('APRENDA PYTHON')
 título('GUSTAVO GUANABARA')
+
+# Programa Principal 3
+contador(2, 1, 7)
+contador(8, 0)
+contador(4, 4, 7, 6, 2)
+
+# Programa Principal 4
+valores = [7, 2, 5, 0, 4]
+print(valores)
+dobra(valores)
+print(valores)
